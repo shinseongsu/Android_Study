7cb40bc4cfa1706dc9bc6d0b1ca8583482afe741
Shin seongsu <tlstjdtn321@naver.com>
2021-01-16 23:03:02 +0900

    autoLink 로 URL 링크 형태로 출력

---
+++ ch1/part2_3/src/main/java/com/example/part2_3/Lab3_7Activity.java
@@ -0,0 +1,16 @@
+package com.example.part2_3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Typeface;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class Lab3_7Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lab3_7);
+    }
+}
