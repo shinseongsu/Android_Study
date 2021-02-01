44edbd66555fe8b18caa63f956d172a0da721d91
Shin seongsu <tlstjdtn321@naver.com>
2021-02-01 22:01:46 +0900

    AsynTask 사용해보기

---
+++ ch13/app/src/main/java/com/example/myapplication/Main2Activity.java
@@ -0,0 +1,83 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.os.SystemClock;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class Main2Activity extends AppCompatActivity implements View.OnClickListener {
+
+    ImageView startView;
+    ImageView pauseView;
+    TextView textView;
+
+    boolean isFirst = true;
+
+    MyAsyncTask asyncTask;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+        startView = (ImageView) findViewById(R.id.main_startBtn);
+        pauseView = (ImageView) findViewById(R.id.main_pauseBtn);
+        textView = (TextView) findViewById(R.id.main_textView);
+
+        startView.setOnClickListener(this);
+        pauseView.setOnClickListener(this);
+
+        asyncTask = new MyAsyncTask();
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v==startView){
+            if(isFirst){
+                asyncTask.isRun=true;
+                asyncTask.execute();
+                isFirst=false;
+            }else {
+                asyncTask.isRun=true;
+            }
+        }else if(v==pauseView){
+            asyncTask.isRun=false;
+        }
+    }
+
+    class MyAsyncTask extends AsyncTask<Void, Integer, String> {
+        boolean loopFlag=true;
+        boolean isRun;
+
+        @Override
+        protected String doInBackground(Void... voids) {
+            int count=10;
+            while (loopFlag){
+                SystemClock.sleep(1000);
+                if(isRun){
+                    count--;
+                    publishProgress(count);
+                    if(count==0){
+                        loopFlag=false;
+                    }
+                }
+            }
+            return "Finish!!";
+        }
+
+        @Override
+        protected void onProgressUpdate(Integer... values) {
+            textView.setText(String.valueOf(values[0]));
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            textView.setText(s);
+        }
+    }
+
+}
